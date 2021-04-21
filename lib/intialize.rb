5d27f4608f85aa0dea9245a9f324dbfc46f65296
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-04-21 23:49:14 +0200

    update instance method

--- lib/intialize.rb
+++ lib/intialize.rb
@@ -1,21 +1,26 @@
 require_relative('../lib/stylint')
 
 class Linters
-     include Stylint
+  include Stylint
 
-     def initialize(file_path)
-          @file_path = file_path
-          @error = [] 
+  def initialize(file_path)
+    @file_path = file_path
+    @errors = []
+  end
 
-     end
-     
-     def check_pass
-          num = 0
-          File.readlines(@file_path).each do |line|
-               num += 1
+  def check_pass
+    num = 0
+    File.readlines(@file_path).each do |_line|
+      num += 1
+      capital_check(line, num)
+    end
 
-
-          end
-        
-     end
+    if @errors.length.zero?
+      @errors.push("All checks have succefully passed ! No errors detected. ").colorize(:light_green)
+    else
+      @errors.sort do|ele1, ele2| 
+       ele2 <=> ele1 
+      end
+    end
+  end
 end
