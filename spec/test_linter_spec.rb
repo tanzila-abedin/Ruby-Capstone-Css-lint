821aa445a741a14f89932d63bc70ee801972375d
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-04-22 16:34:39 +0200

    add working test for selectors hyphen method

--- spec/test_linter_spec.rb
+++ spec/test_linter_spec.rb
@@ -2,6 +2,22 @@ require_relative '../lib/intialize'
 require_relative '../lib/stylint'
 
 describe Linters do
-     subject(:check) {Linters.new('../assets/style.css')}
+     let(:check) { Linters.new('../assets/style.css')}
 
+     describe 'selectors_hyphen_check' do
+       context 'check for camelcasing' do
+          it 'should check when camelcasing is used' do
+               result = check.send(:selectors_hyphen_check,'/([A-Z][a-z0-9]+){2,}/', 3 )
+               expect(result).not_to eq(['camelcasing  is used on line number 3'])
+          end
+          # it 'should check when camelcasing is not used' do
+          #       result = check.send(:selectors_hyphen_check,'/([A-Z][a-z0-9]+){2,}/', 3 )
+          #      expect(result).to eq('-')
+          # end
+     end
+         
+     end
+
+
+    
 end
