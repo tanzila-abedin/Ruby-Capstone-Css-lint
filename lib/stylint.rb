1231200c2f08a04e45cc63b20b071b4d991b54ec
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-04-22 23:22:34 +0200

    update display error messages

--- lib/stylint.rb
+++ lib/stylint.rb
@@ -2,31 +2,31 @@ module Stylint
    # Selectors must be on a new line, multiple selectors on a line are not allowed. WORKING!!!!!!!!!!!!!
   def selectors_newline_check(line, num) 
   # return unless line.count('word') > 1 #is not
-    @errors << "Selectors must be on a new line, avoid using multiple line #{num}".colorize(:light_red) if line.start_with?('#' || '.')
+    @errors << "Line #{num}: Selectors must be on a new line, avoid using multiple".colorize(:light_red) if line.start_with?('#' || '.')
   end
 
   # Must use hyphens to separate words, not underscores or camel casing.  WORKING!!!!!!!!!!!!!!!!
   def selectors_hyphen_check(line, num)
-    @errors << "use hyphens to separate words, not underscore or camel casing on line #{num}".colorize(:light_red) if line =~  /([A-Z][a-z0-9]+){2,}/
+    @errors << "Line #{num} : use hyphens to separate words, not underscore or camel casing on line".colorize(:light_red) if line =~  /([A-Z][a-z0-9]+){2,}/
     # /([A-Z]*_?[A-Z]*)*/ 
     # || line =~ /^[A-Z]\w+(?:[A-Z]\w+){1,}/x
   end
 
   def double_braces_check(line, num)
     # strip = line.delete("\n")
-    @errors << "#{'ERROR'.red} :Line #{num} Double OPENING or CLOSING braces detected \n"  if line.include?('{{') || line.include?('}}') 
+    @errors << "#{'ERROR'.red} :Line #{num}: Double OPENING or CLOSING braces detected \n"  if line.include?('{{') || line.include?('}}') 
   end
 
   def double_bracket_check(line, num)
-   @errors << "#{'ERROR'.red} :Line #{num} Double OPENING or CLOSING bracket detected \n"  if line.include?('((') || line.include?('))') 
+   @errors << "#{'ERROR'.red} :Line #{num} : Double OPENING or CLOSING bracket detected \n"  if line.include?('((') || line.include?('))') 
   end
 
   # Include one space after the colon for each declaration
   
-  def space_after_colon(line, number)
+  def space_after_colon(line, num)
     return unless line.include?(':')
     miss = line.split(':')[1]
-    @errors << "#{'ERROR'.red} : A single SPACE expected after the colon on line #{number}\n" if miss[0] != ' '
+    @errors << "#{'ERROR'.red} : Line #{num}: A single SPACE expected after the colon.\n" if miss[0] != ' '
   end
 
   # End all declarations with a semi-colon.
@@ -41,12 +41,12 @@ module Stylint
   end
 
   def invalid_hexcode_check(line, num)
-    @errors << "#{'WARNING'.yellow} :Line #{num} wrong hexcode" if line.include?('#00' || '#fff1az' || '#12345aa')
+    @errors << "#{'WARNING'.yellow} :Line #{num} The hexcode does not exist" if line.include?('#00' || '#fff1az' || '#12345aa')
   end
 
   def font_check (line,num)
    return unless line.include?('font-weight') && !line.match(/\d/i)
-   @errors << "The font weight should be a number on the line number #{num}".colorize(:red)
+   @errors << "Line #{num} : The font weight should be a number".colorize(:red)
   end
 
   # def duplicate_font_check(line,num)
@@ -54,16 +54,5 @@ module Stylint
   #   # each { |ele1,ele2| ele1 == ele2}
   #   end
 
-  # def capital_check(line, num)
-  #  @errors << "Replace capital letters with lowercase on line #{num}"
-  #  .colorize(:light_red) if line =~ /[A-Z]/
-  # end
-
-  # # Lowercase all hex values, e.g. #eee.
-  # def hex_lowcase_check(line, num)
-  #   return unless  line.include?.gsub(/^[a-z0-9_\-]+$/) #return if not
-  #   @errors << "Replace the capital letters with lower case letters on the line  #{num}".colorize(:light_red) 
-  #   if line =~ /^[a-z0-9_\-]+$/
-  # end
 
 end
